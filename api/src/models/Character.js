fd4bd24476c303a950c679d3a0d3cf9c8646980e
Chae Mendoza <israelmendoza656@outlook.com>
2022-09-19 18:53:20 -0500

    Modelos y relaciones

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,6 +4,27 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
-
+    id:  {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.STRING,
+    },
+    origin: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.TEXT,
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
+const axios = require('axios');
+const Character = require("../db");
 
 const router = Router();
 
 // Configurar los routers
+const apiInfo = axios.get("https://rickandmortyapi.com/api/character")
 
 module.exports = router;
