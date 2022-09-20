903bf34a5236fcffd16cd9181352ba0a094b54e7
Chae Mendoza <israelmendoza656@outlook.com>
2022-09-19 19:36:55 -0500

    Avance en la ruta del get!

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,59 @@
 const { Router } = require("express");
 const axios = require('axios');
-const Character = require("../db");
+const {Character, Episode} = require("../db");
 
 const router = Router();
 
 // Configurar los routers
-const apiInfo = axios.get("https://rickandmortyapi.com/api/character")
+const getAllCharactersApi = async ()=> {
+    const apiInfo = axios.get("https://rickandmortyapi.com/api/character");
+    const character = apiInfo.data.results.map(e => {
+        return {
+            id: e.id,
+            name: e.name,
+            species: e.species,
+            origin: e.origin,
+            image: e.image
+        }
+    })
+    return character
+}
+
+const getDbInfo = async () => {
+    const dbInfo = await Character.findAll({
+        include: [
+            {
+                model: Episode,
+                attributes: ["name"],
+                through: {
+                    attributes: []
+                }
+            }
+        ]
+    });
+    return dbInfo;
+}
+
+const getAllCharacters = async () => {
+    const apiInfo = await getAllCharactersApi();
+    const dbInfo = await getDbInfo();
+    return apiInfo.concat(dbInfo)
+}
+
+router.get("/character", async (req, res) => {
+    try {
+        const name = req.query.name
+        let charactersTotal = await getAllCharacters();
+        if (name) {
+            let characterName = await charactersTotal.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
+            characterName ? res.status(200).send(characterName) : res.status(404).send("El personaje no fue encontrado, lo siento.")
+        } else {
+            res.status(200).send(charactersTotal)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+})
 
 module.exports = router;
